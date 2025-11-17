c1d1f87b61a5ffa69bda9c4a3017d9aa0a03443e
Andrew Bates <a@sprock.io>
2025-11-17 11:37:23 -0600

    Refactor benchmark functions to use ProcessFrame instead of AddFrame

--- events_bench_test.go
+++ events_bench_test.go
@@ -9,18 +9,26 @@ import (
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func BenchmarkEventDetector_AddFrame(b *testing.B) {
+func BenchmarkEventDetector_ProcessFrame(b *testing.B) {
 	detector := NewEventDetector()
+	defer detector.Stop()
+
 	frame := createPostMatchTestFrame("playing", 1, 0)
+
+	// Drain events channel in background
+	go func() {
+		for range detector.EventsChan() {
+		}
+	}()
 
 	b.ReportAllocs()
 
 	for b.Loop() {
-		detector.AddFrame(frame)
+		detector.ProcessFrame(frame)
 	}
 }
 
-func BenchmarkEventDetector_AddFrame_WithTransition(b *testing.B) {
+func BenchmarkEventDetector_ProcessFrame_WithTransition(b *testing.B) {
 	frames := []*rtapi.LobbySessionStateFrame{
 		createPostMatchTestFrame("playing", 2, 1),
 		createPostMatchTestFrame("post_match", 3, 1),
@@ -30,19 +38,35 @@ func BenchmarkEventDetector_AddFrame_WithTransition(b *testing.B) {
 
 	for b.Loop() {
 		detector := NewEventDetector()
+
+		// Drain events channel in background
+		go func() {
+			for range detector.EventsChan() {
+			}
+		}()
+
 		for _, frame := range frames {
-			detector.AddFrame(frame)
+			detector.ProcessFrame(frame)
 		}
+
+		detector.Stop()
 	}
 }
 
-func BenchmarkEventDetector_AddFrame_FullBuffer(b *testing.B) {
+func BenchmarkEventDetector_ProcessFrame_FullBuffer(b *testing.B) {
 	detector := NewEventDetector()
+	defer detector.Stop()
+
+	// Drain events channel in background
+	go func() {
+		for range detector.EventsChan() {
+		}
+	}()
 
 	// Fill the buffer to capacity
-	for i := 0; i < detector.frameCount; i++ {
+	for i := 0; i < MaxFrameBufferCapacity; i++ {
 		frame := createPostMatchTestFrame("playing", int32(i%3), int32(i%2))
-		detector.AddFrame(frame)
+		detector.ProcessFrame(frame)
 	}
 
 	frame := createPostMatchTestFrame("playing", 1, 0)
@@ -50,11 +74,11 @@ func BenchmarkEventDetector_AddFrame_FullBuffer(b *testing.B) {
 	b.ReportAllocs()
 
 	for b.Loop() {
-		detector.AddFrame(frame)
+		detector.ProcessFrame(frame)
 	}
 }
 
-func BenchmarkEventDetector_AddFrame_Sequence(b *testing.B) {
+func BenchmarkEventDetector_ProcessFrame_Sequence(b *testing.B) {
 	// Pre-create all frames before timing
 	frames := make([]*rtapi.LobbySessionStateFrame, 100)
 	for j := 0; j < 100; j++ {
@@ -88,9 +112,17 @@ func BenchmarkEventDetector_AddFrame_Sequence(b *testing.B) {
 	for b.Loop() {
 		detector := NewEventDetector()
 
+		// Drain events channel in background
+		go func() {
+			for range detector.EventsChan() {
+			}
+		}()
+
 		// Simulate a realistic sequence of frames
 		for _, frame := range frames {
-			detector.AddFrame(frame)
+			detector.ProcessFrame(frame)
 		}
+
+		detector.Stop()
 	}
 }
